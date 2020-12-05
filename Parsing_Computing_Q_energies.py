d9c8b4cea3ccc42859499effa0f9fbdc0f6fef93
nouraldinkahlous <nouraldinkahlous@gmail.com>
2020-12-05 22:50:24 +0100

    Zwanzig_all_functions

--- Parsing_Computing_Q_energies.py
+++ Parsing_Computing_Q_energies.py
@@ -56,73 +56,82 @@ def createDataFrames(rawEnergy):
 
     return pd.DataFrame(rawEnergy, columns=Columns_name)
 
+
+#%%
+def dE_Calculation2(steps):
+    
+    dEs=pd.DataFrame()
+    Energies_df=(pd.DataFrame({"State_A_Lambda":State_A_df["Lambda"],"State_A_G":State_A_df["Q_sum"] ,"State_B_Lambda":State_B_df["Lambda"],"State_B_G":State_B_df["Q_sum"],"E":State_B_df["Q_sum"] - State_A_df["Q_sum"] })).sort_values('State_A_Lambda')
+    Energies_df.iloc[:1]
+    State_A_Energies_dict=dict(Energies_df.groupby('State_A_Lambda',sort=False)['State_A_G'].apply(list))
+    State_B_Energies_dict=dict(Energies_df.groupby('State_B_Lambda',sort=False)['State_B_G'].apply(list)) 
+
+    
+    for i in range(len(State_A_Energies_dict.keys())-1):
+        State_A_Energies = (pd.DataFrame.from_dict({((list(State_A_Energies_dict.keys())[i])):(State_A_Energies_dict.get(list(State_A_Energies_dict.keys())[i])),((list(State_A_Energies_dict.keys())[i+1])):(State_A_Energies_dict.get(list(State_A_Energies_dict.keys())[i+1]))}, orient='index').transpose()).iloc[:steps]
+        State_B_Energies = (pd.DataFrame.from_dict({((list(State_B_Energies_dict.keys())[i])):(State_B_Energies_dict.get(list(State_B_Energies_dict.keys())[i])),((list(State_B_Energies_dict.keys())[i+1])):(State_B_Energies_dict.get(list(State_B_Energies_dict.keys())[i+1]))}, orient='index').transpose()).iloc[:steps]
+        
+        State_A_Lambda_float=State_A_Energies.columns
+        State_B_Lambda_float=State_B_Energies.columns
+        
+        E0=(State_A_Energies*State_A_Lambda_float).values+(State_B_Energies*State_B_Lambda_float).values
+        E1=(State_A_Energies*list((State_A_Lambda_float).values)[::-1]).values+(State_B_Energies*list((State_B_Lambda_float).values)[::-1]).values
+        dE=pd.DataFrame(E1-E0,columns=[str(State_A_Lambda_float.values[0])+"_"+str(State_B_Lambda_float.values[0])+"-"+str(State_A_Lambda_float.values[1])+"_"+str(State_B_Lambda_float.values[1]),str(State_A_Lambda_float.values[1])+"_"+str(State_B_Lambda_float.values[1])+"-"+str(State_A_Lambda_float.values[0])+"_"+str(State_B_Lambda_float.values[0])])
+        dEs=dEs.append(dE.transpose())
+    
+    return dEs.transpose()
+
+#%%
+def Zwnazig_Estimator(dEs_df):
+    dEs_df=pd.DataFrame(-0.592*np.log(np.mean(np.exp(-dEs/0.592))))
+    Lambdas=[]
+    dGF=[]
+    dGF_sum=[]
+    dGR=[]
+    dGR_sum=[]
+    dG_Average=[]
+    dGR.append(0.0)
+    dG_Average.append(0.0)
+
+    for i in range(1,len(dEs_df.index),2):
+        Lambdas.append(re.split('_|-',dEs_df.index[i-1])[1])
+        dGF.append(dEs_df.iloc[i,0])
+        dGR.append(dEs_df.iloc[i-1,0])
+    Lambdas.append(re.split('_|-',dEs_df.index[-1])[1])
+    dGF.append(0.0)
+    dGF=dGF[::-1]
+    for i in range(len(dGF)):
+        dGF_sum.append(sum(dGF[:i+1]))
+        dGR_sum.append(sum(dGR[:i+1]))
+
+    dG_average_raw=((pd.DataFrame(dGF[1:]))-pd.DataFrame(dGR[1:][::-1]))/2
+
+    for i in range(len(list(dG_average_raw.values))):
+        dG_Average.append(np.sum(dG_average_raw.values[:i+1]))
+
+    Zwanzig_df=pd.DataFrame.from_dict({"Lambda":Lambdas,"dG_Forward":dGF,"SUM_dG_Forward":dGF_sum,"dG_Reverse":dGR[::-1],"SUM_dG_Reverse":dGR_sum[::-1],"dG_Average":dG_Average})
+    Zwanzig_final_dG = Zwanzig_df['dG_Average'].iloc[-1]
+    return Zwanzig_df, Zwanzig_final_dG
+
 #%%
 
 #if '__name__' == '__main__':
 
 #%%
-    #os.chdir("/Users/nour/New_qfep") #MAC
-    os.chdir("Z:/jobs/Qfep_NEW/")
-    EnergyFiles_Lst = [filename for filename in glob.glob("FEP3*.en")]  
+    #os.chdir("/Users/nour/New_qfep/qfep_small") #MAC
+    os.chdir("Z:/jobs/Qfep_NEW/qfep_small")
+    EnergyFiles_Lst = [filename for filename in glob.glob("FEP*.en")]  
     State_A_RawEnergies_Lst, State_B_RawEnergies_Lst = ReadBinary(EnergyFiles_Lst)
     State_A_df = createDataFrames(State_A_RawEnergies_Lst)
     State_B_df = createDataFrames(State_B_RawEnergies_Lst)
+    dEs =  dE_Calculation2(None)
+    Zwanzig_df, Zwanzig_final_dG= Zwnazig_Estimator(dEs)
 
 
 
 
-
-##Zwnazig
-# %%
-
-#Zwanzig_exp= -0.592*(np.log(np.mean(np.exp((State_A_df["Lambda"]*State_A_df["Q_sum"] + State_B_df["Lambda"]*State_B_df["Q_sum"])*0.592))))
-#Zwanzig_exp
-
-
-
-# %%
-# Energies_df=pd.DataFrame({"State_A_Lambda":State_A_df["Lambda"],"State_A_G":State_A_df["Q_sum"] ,"State_B_Lambda":State_B_df["Lambda"],"State_B_G":State_B_df["Q_sum"],"E":State_B_df["Q_sum"] - State_A_df["Q_sum"] })
-
-# %%
-# -0.592*(np.log(np.mean(np.exp((-27.05200000000002)*-0.592))))
-
-# %%
-
-# E0= State_A_df["Q_sum"]*State_A_df["Lambda"]+State_B_df["Q_sum"]*State_B_df["Lambda"]
-# E1= State_A_df["Q_sum"]*State_B_df["Lambda"]+State_B_df["Q_sum"]*State_A_df["Lambda"]
-
-
-# dE=E1-E0
-# dE= np.exp(-dE/0.592)
-
-# lambdas_dE=[]
-# for i in range(0,len(dE),int(len(dE)/2)):
-#     lambdas_dE.append(i)
-
-# zz = [sum(dE[i:i+int(len(dE)/2)])/len(dE[i:i+int(len(dE)/2)]) for i in lambdas_dE]
-# # for i in lambdas_dE:
-
-# #     z.(sum(dE[i:i+int(len(dE)/2)])/len(dE[i:i+int(len(dE)/2)]))
-    
-# dg=-0.592*np.log(zz)
-# dg
-# %%
-
-
-# %%
-# Energies_df=pd.DataFrame({"State_A_Lambda":State_A_df["Lambda"],"E":State_B_df["Q_sum"] - State_A_df["Q_sum"] })
-
-# %%
-
-# A=pd.DataFrame(dict(Energies_df.groupby('State_A_Lambda',sort=False)['State_A_G'].apply(list)))
-# B=pd.DataFrame(dict(Energies_df.groupby('State_B_Lambda',sort=False)['State_B_G'].apply(list)))
-
-# E0=(A*A.columns).values+(B*B.columns).values
-# E1=(A*list((A.columns).values)[::-1]).values+(B*list((B.columns).values)[::-1]).values
-# dE=pd.DataFrame(E1-E0)
-
-# dG=pd.DataFrame(-0.592*np.log(np.mean(np.exp(-pd.DataFrame(E1-E0)/0.592))))
-
+##dEs
+#%%
 
 # %%  
 Energies_df=pd.DataFrame({"State_A_Lambda":State_A_df["Lambda"],"State_A_G":State_A_df["Q_sum"] ,"State_B_Lambda":State_B_df["Lambda"],"State_B_G":State_B_df["Q_sum"],"E":State_B_df["Q_sum"] - State_A_df["Q_sum"] })
@@ -158,6 +167,8 @@ for i in range(1,len(dG),2):
 Zwanzig_df=pd.DataFrame.from_dict({"dGF":dGF,"dGR":dGR})
 Zwanzig_exp=-np.mean(abs(np.sum((Zwanzig_df))))
 Zwanzig_exp
+
+
 # %%
 #def Zwnazig(dEs_df):
 dEs_df=pd.DataFrame(-0.592*np.log(np.mean(np.exp(-dEs/0.592))))
@@ -190,7 +201,6 @@ for i in range(len(list(dG_average_raw.values))):
 #Zwanzig_df=pd.DataFrame.from_dict({"Lambda":Lambdas,"dG_Forward":dGF,"SUM_dG_Forward":dGF_sum,"dG_Reverse":dGR[::-1]})
 Zwanzig_df=pd.DataFrame.from_dict({"Lambda":Lambdas,"dG_Forward":dGF,"SUM_dG_Forward":dGF_sum,"dG_Reverse":dGR[::-1],"SUM_dG_Reverse":dGR_sum[::-1],"dG_Average":dG_Average})
 
-Zwanzig_df
+Zwanzig_df['dG_Average'].iloc[-1]
 
 # %%
-
