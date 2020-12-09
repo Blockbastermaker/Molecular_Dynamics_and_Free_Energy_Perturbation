e059599bbdb7e1104cfaf7a0bb65c8d0d3df0538
nouraldinkahlous <nouraldinkahlous@gmail.com>
2020-12-09 20:36:10 +0100

    Clean_Code2

--- Parsing_Computing_Q_energies.py
+++ Parsing_Computing_Q_energies.py
@@ -9,42 +9,8 @@ import re
 #%%
 
 
-def ReadBinary(EnergyFiles_Lst):
-
-    State_A_RawEnergies = []
-    State_B_RawEnergies = []
-
-    for file in EnergyFiles_Lst:
-        with open(file,'rb') as f: fileContent = f.read()
-
-        Version_Check_Str=str(list(struct.unpack("c" * ((len(fileContent[32:112]))//1),fileContent[32:112]))).replace("b'", "").strip("[],' '").replace("'","").replace(",","").replace(" ","")
-  
-        EnergyFileLength_int=len(fileContent)
-        BinaryChankSize_int=120
-        NextBinaryChank_int=272
-        HeaderSize_int=124
-        State_B_Shift_int=132
-
-        ## Check Q_Energies Version !!!
-
-        if "6." in Version_Check_Str: HeaderSize_int += 4
-        
-        if not '5.' in Version_Check_Str and not "6." in Version_Check_Str:
-            print("Pleaes Check the your Qdyn verion in file: "+file +" ----> format is NOT Supported !!! ")
-            exit()
-            
-  
-
-        for Byte in range(HeaderSize_int, EnergyFileLength_int, NextBinaryChank_int):
-            State_A_Lst=struct.unpack("d" * ((len(fileContent[Byte:Byte+BinaryChankSize_int]))//8),fileContent[Byte:Byte+BinaryChankSize_int])
-            State_B_Lst=struct.unpack("d" * ((len(fileContent[Byte+State_B_Shift_int:Byte+State_B_Shift_int+BinaryChankSize_int]))//8),fileContent[Byte+State_B_Shift_int:Byte+State_B_Shift_int+BinaryChankSize_int])
-            State_A_RawEnergies.append(State_A_Lst)
-            State_B_RawEnergies.append(State_B_Lst)
-        
-    return State_A_RawEnergies, State_B_RawEnergies
-
 #%%
-def ReadBinary2(EnergyFiles_Lst):
+def ReadBinary(EnergyFiles_Lst):
     
     State_A_RawEnergies = []
     State_B_RawEnergies = []
@@ -159,14 +125,15 @@ def Plot_dG(df):
 #if '__name__' == '__main__':
 
 #%%
-    os.chdir("/Users/nour/New_qfep/") #MAC
+    #os.chdir("/Users/nour/New_qfep/") #MAC
     #os.chdir("Z:/jobs/Qfep_NEW/qfep_small/test")"
     #os.chdir("G:/PhD/Project/En")
     
     EnergyFiles_Lst = [filename for filename in glob.glob("FEP*.en")]  
-    State_A_RawEnergies_Lst, State_B_RawEnergies_Lst = ReadBinary2(EnergyFiles_Lst)
+    State_A_RawEnergies_Lst, State_B_RawEnergies_Lst = ReadBinary(EnergyFiles_Lst)
     State_A_df = createDataFrames(State_A_RawEnergies_Lst)
     State_B_df = createDataFrames(State_B_RawEnergies_Lst)
     dEs =  dE_Calculation2(None)
     Zwanzig_df, Zwanzig_Final_dG= Zwnazig_Estimator(dEs)
     Plot_dG(Zwanzig_df)
+# %%
