47616b9a424b0d1bc9c75b703cd0f479a30d27d1
nouraldinkahlous <nouraldinkahlous@gmail.com>
2021-02-21 20:28:49 +0100

    bugfix11

--- fepanalysis/feptool.py
+++ fepanalysis/feptool.py
@@ -52,8 +52,7 @@ def main():
     if args.convergence_analysis is not None:
 
         args.convergence_analysis=args.convergence_analysis[0].split(',')
-        e=args.convergence_analysis[0]
-        convergenc_df = tools.Convergence(dEs,eval('estimators.Estimators.'+e), int(args.convergence_analysis[1]), int(args.convergence_analysis[2]),10)
+        convergenc_df = tools.Convergence(dEs,estimators.Estimators.Zwanzig, int(args.convergence_analysis[1]), int(args.convergence_analysis[2]), int(args.convergence_analysis[3]))
 
         print(convergenc_df)
         
