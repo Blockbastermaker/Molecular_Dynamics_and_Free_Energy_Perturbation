04c8129b8296dad04da5d01ac3a85d90415132e9
nouraldinkahlous <nouraldinkahlous@gmail.com>
2021-02-21 19:05:40 +0100

    fix convergance bug

--- fepanalysis/tools.py
+++ fepanalysis/tools.py
@@ -1,8 +1,9 @@
+from fepanalysis.estimators import Estimators
 import pandas as pd
-
+ 
 def Convergence(df,Estimator,StepsChunk_Int,ReplicatiesCount_Int,EnergyOutputInterval_Int):
                                     # the last and first steps are not included in the reading
-    Zwanzig_Final_Lst=[Estimator(df,steps_limit)[1] for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df)+1,StepsChunk_Int*ReplicatiesCount_Int)]
+    Zwanzig_Final_Lst=[Estimators.Estimator(df,steps_limit)[1] for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df)+1,StepsChunk_Int*ReplicatiesCount_Int)]
     StepsChunk_Lst=[EnergyOutputInterval_Int*steps_limit/ReplicatiesCount_Int for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df)+1,StepsChunk_Int*ReplicatiesCount_Int)]
     Convergence_df=pd.DataFrame({'Number of Steps':StepsChunk_Lst, 'dG':Zwanzig_Final_Lst })
     return Convergence_df
