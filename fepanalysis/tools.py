204ce96b7483646b52fb7219458f9e852fb81d00
nouraldinkahlous <nouraldinkahlous@gmail.com>
2021-02-22 00:07:05 +0100

    fix26

--- fepanalysis/tools.py
+++ fepanalysis/tools.py
@@ -1,13 +1,6 @@
 from . import estimators
 import pandas as pd
-from .inout import commands
-from .inout import Q
-from . import estimators
-from . import tools
-from . import plots
-import os
-import glob
-
+ 
 def Convergence(df,Estimator,StepsChunk_Int,ReplicatiesCount_Int,EnergyOutputInterval_Int):
                                     # the last and first steps are not included in the reading
     Zwanzig_Final_Lst=[eval('estimators.Estimators.'+Estimator)(df,steps_limit)[1] for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df)+1,StepsChunk_Int*ReplicatiesCount_Int)]
