262a8873a701b15153d6f1eee094af3bd6566f2e
nouraldinkahlous <nouraldinkahlous@gmail.com>
2021-02-22 01:06:25 +0100

    fix44

--- fepanalysis/estimators.py
+++ fepanalysis/estimators.py
@@ -139,7 +139,10 @@ class Estimators():
 
     def Convergence(df,Estimator,StepsChunk_Int,ReplicatiesCount_Int,EnergyOutputInterval_Int):
                                         # the last and first steps are not included in the reading
-        Zwanzig_Final_Lst=[eval('Estimators.'+Estimator)(df,steps_limit)[1] for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df)+1,StepsChunk_Int*ReplicatiesCount_Int)]
+        if Estimator=='Zwanzig':
+            Estimator=Estimators.Zwanzig
+            
+        Zwanzig_Final_Lst=[Estimator(df,steps_limit)[1] for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df)+1,StepsChunk_Int*ReplicatiesCount_Int)]
         StepsChunk_Lst=[EnergyOutputInterval_Int*steps_limit/ReplicatiesCount_Int for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df)+1,StepsChunk_Int*ReplicatiesCount_Int)]
         Convergence_df=pd.DataFrame({'Number of Steps':StepsChunk_Lst, 'dG':Zwanzig_Final_Lst })
         return Convergence_df
