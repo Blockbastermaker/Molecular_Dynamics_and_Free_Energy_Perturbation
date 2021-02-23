e0540f648837e750520ab4df237b0ddccd48e4d9
nouraldinkahlous <nouraldinkahlous@gmail.com>
2021-02-23 15:51:57 +0100

    TI convergance

--- fepanalysis/feptool.py
+++ fepanalysis/feptool.py
@@ -49,14 +49,14 @@ def main():
         TI_dG = estimators.Estimators.TI(State_A_df, State_B_df,None)[1]
         print("ZW: ",Zwanzig_dG)
         print("TI: ", TI_dG)
-        u_nk_df= estimators.Estimators.Create_df_BAR_MBAR(State_A_df, State_B_df)
-        BAR_df= estimators.BAR().fit(u_nk_df)
-        print('BAR: ',BAR_df.delta_f_.loc[0.00, 1.00])
+        # u_nk_df= estimators.Estimators.Create_df_BAR_MBAR(State_A_df, State_B_df)
+        # BAR_df= estimators.BAR().fit(u_nk_df)
+        # print('BAR: ',BAR_df.delta_f_.loc[0.00, 1.00])
         
-        from pymbar import MBAR as MBAR_
-        from alchemlyb.estimators import MBAR
-        MBAR_df= MBAR().fit(u_nk_df)
-        print('MBAR: ',MBAR_df.delta_f_.loc[0.00, 1.00])
+        # from pymbar import MBAR as MBAR_
+        # from alchemlyb.estimators import MBAR
+        # MBAR_df= MBAR().fit(u_nk_df)
+        # print('MBAR: ',MBAR_df.delta_f_.loc[0.00, 1.00])
         
     if args.convergence_analysis is not None:
         args.convergence_analysis=args.convergence_analysis[0].split(',')
