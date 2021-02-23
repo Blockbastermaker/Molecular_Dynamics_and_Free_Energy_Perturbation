fce19d4b2d6a3838bfb465812f16726d3f7da475
nouraldinkahlous <nouraldinkahlous@gmail.com>
2021-02-23 13:35:59 +0100

    Convergance fix2

--- fepanalysis/feptool.py
+++ fepanalysis/feptool.py
@@ -42,9 +42,9 @@ def main():
 
     if args.estimator =='Zwanzig_Estimator':
 
-        DG_df = estimators.Estimators.Zwanzig(dEs,None)
+        DG_df, Zwanzig_dG= estimators.Estimators.Zwanzig(dEs,None)
         dU_dH_df=estimators.Estimators.Create_df_TI(State_A_df, State_B_df)
-        Zwanzig_dG = DG_df['dG_Average'].iloc[-1]
+        #Zwanzig_dG = DG_df['dG_Average'].iloc[-1]
         TI_dG = estimators.Estimators.TI(dU_dH_df)
         print("ZW: ",Zwanzig_dG )
         print("TI: ", TI_dG)
