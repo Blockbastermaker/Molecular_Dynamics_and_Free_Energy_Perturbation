cfc2fdbe6bf4a49c54787cfb27e2d0b6bbf24238
nouraldinkahlous <nouraldinkahlous@gmail.com>
2021-02-26 00:40:13 +0100

    BAR2

--- fepanalysis/estimators.py
+++ fepanalysis/estimators.py
@@ -343,7 +343,7 @@ class Estimators():
             dGs_Lst=[Estimator(df1,df2,steps_limit)[1] for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,int((len(df1)/len(df1['Lambda'].unique())))+1,StepsChunk_Int*ReplicatiesCount_Int)]
             StepsChunk_Lst=[EnergyOutputInterval_Int*steps_limit/ReplicatiesCount_Int for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,int((len(df1)/len(df1['Lambda'].unique())))+1,StepsChunk_Int*ReplicatiesCount_Int)]
 
-        if isinstance(df2, pd.DataFrame) and not isinstance(df1, pd.DataFrame):
+        elif isinstance(df2, pd.DataFrame) and not isinstance(df1, pd.DataFrame):
             
             dGs_Lst=[Estimator(df1,df2,steps_limit)[1] for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df2)+1,StepsChunk_Int*ReplicatiesCount_Int)]
             StepsChunk_Lst=[EnergyOutputInterval_Int*steps_limit/ReplicatiesCount_Int for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df2)+1,StepsChunk_Int*ReplicatiesCount_Int)]
