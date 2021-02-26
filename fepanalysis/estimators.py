7de362e56a8fecd699b72ecd8591f60cfd028def
nouraldinkahlous <nouraldinkahlous@gmail.com>
2021-02-26 01:11:44 +0100

    BAR2

--- fepanalysis/estimators.py
+++ fepanalysis/estimators.py
@@ -280,7 +280,6 @@ class Estimators():
         lambdas_df=lambdas_list_A
         for x in States_dicts.keys():
             for i in range(len(States_dicts[x])):
-                print(i, x)
                 States_dicts[x][i]=States_dicts[x][i][:steps]
                 
         for i in range(len(States_dicts)):
@@ -344,8 +343,7 @@ class Estimators():
             StepsChunk_Lst=[EnergyOutputInterval_Int*steps_limit/ReplicatiesCount_Int for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,int((len(df1)/len(df1['Lambda'].unique())))+1,StepsChunk_Int*ReplicatiesCount_Int)]
 
         elif isinstance(df2, pd.DataFrame) and not isinstance(df1, pd.DataFrame):
-            print(len(df2))
-            dGs_Lst=[Estimator(df1,df2,steps_limit)[1] for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df2)+1,StepsChunk_Int*ReplicatiesCount_Int)]
+            dGs_Lst=[Estimator(df1,df2,steps_limit) for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df2)+1,StepsChunk_Int*ReplicatiesCount_Int)]
             StepsChunk_Lst=[EnergyOutputInterval_Int*steps_limit/ReplicatiesCount_Int for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df2)+1,StepsChunk_Int*ReplicatiesCount_Int)]
         else:
             dGs_Lst=[Estimator(df1,steps_limit)[1] for steps_limit in range((StepsChunk_Int-2)*ReplicatiesCount_Int,len(df1)+1,StepsChunk_Int*ReplicatiesCount_Int)]
